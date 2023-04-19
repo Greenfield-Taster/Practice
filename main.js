d5ec980898508ca774c7ef75f829adb28b8f53e8
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2023-04-20 01:24:08 +0300

    Changed tomato-all to grapefruit

---
+++ main.js
@@ -0,0 +1,49 @@
+var app = new Vue({
+  el: ".tomatos-block",
+  data: {
+    products: [
+      {
+        id: "greip1",
+        title: "Pink grapefruit",
+        image: "./img/greip1.jpg",
+        shortText: "Short description 1",
+        desc: "Full description",
+      },
+      {
+        id: "greip2",
+        title: "White grapefruit",
+        image: "./img/greip2.jpg",
+        shortText: "Short description 2",
+        desc: "Full description",
+      },
+      {
+        id: "greip3",
+        title: "Ruby red grapefruit",
+        image: "./img/greip3.jpg",
+        shortText: "Short description 3",
+        desc: "Full description",
+      },
+      {
+        id: "greip4",
+        title: "Star ruby grapefruit",
+        image: "./img/greip4.jpg",
+        shortText: "Short description 4",
+        desc: "Full description",
+      },
+      {
+        id: "greip5",
+        title: "Oroblanco grapefruit",
+        image: "./img/greip5.jpg",
+        shortText: "Short description 5",
+        desc: "Full description",
+      },
+      {
+        id: "greip6",
+        title: "Marsh red grapefruit",
+        image: "./img/greip5.jpg",
+        shortText: "Short description 6",
+        desc: "Full description",
+      },
+    ],
+  },
+});
---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector("navbar-toggle");
+const Menu = document.querySelector("Menu");
+
+toggleButton.addEventListener("click", () => {
+  Menu.classList.toggle("active");
+});
