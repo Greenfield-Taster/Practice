87f35b20251f8bdc312465b9b64b2e2656e41996
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2023-04-25 22:27:50 +0300

    Add random generation pictures on js

--- script.js
+++
@@ -1,6 +0,0 @@
-// const toggleButton = document.querySelector("navbar-toggle");
-// const Menu = document.querySelector("Menu");
-
-// toggleButton.addEventListener("click", () => {
-//   Menu.classList.toggle("active");
-// });
